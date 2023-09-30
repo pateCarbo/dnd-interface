8b5ee4ac0fa2c84d09928c879ade5d1e2382ff9d
pateCarbo <andy.le-roy@epita.fr>
2023-09-30 19:43:39 +0200

    :sparkles: Registration ok

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -77,7 +77,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setRoles(array $roles): static
     {
         $this->roles = $roles;
-
         return $this;
     }
 
